2a1d312648501558d4b46a3095d0e612212d05ce
Aditya Bansal <bansaladitya@gmail.com>
2024-05-10 18:07:34 +0530

    added something

--- src/Organism/Chat.js
+++ src/Organism/Chat.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Chat = () => {
   return (
-    <div className='flex flex-col overflow-y-auto h-screen'>
-      Chat here
-      
+    <div className='flex flex-col overflow-y-auto h-screen bg-primary-700'>
+      <div className='fixed p-4 bg-secondary-300 w-full'>
+        name
+      </div>
+      <div className='w-full fixed bottom-0 p-3 bg-primary-500 flex justify-center items-center'>
+        
+      <form className='flex items-center w-full'>
+      <input
+      className='rounded p-1 bg-secondary-50 w-[50%] ml-[7%]'
+        type="text"
+        placeholder="Search..."
+      />
+      <button type="submit"><span className="material-symbols-outlined text-white mt-2 ml-3">
+search
+</span></button>
+    </form>
+      </div>
     </div>
   )
 }
--- src/Organism/Topbar.js
+++ src/Organism/Topbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Topbar = () => {
   return (
     <>
-        <div className='flex justify-between p-3 bg-primary-400'>
+        <div className='flex justify-between p-3 bg-primary-400 text-white'>
             <div className=''>Chat-app</div>
             <div>personal note</div>
         </div>
