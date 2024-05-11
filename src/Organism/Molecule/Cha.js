f73377498e48c989ccf65dd6b0da180dfe3a392a
AdityaBansal1402 <bansaladitya3009@gmail.com>
2024-05-11 22:06:46 +0530

    doing it okgit add .!

--- src/Organism/Molecule/Cha.js
+++ src/Organism/Molecule/Cha.js
@@ -2,14 +2,22 @@ import React from 'react'
 
 const Cha = () => {
   return (
-    <>
-    <div className='mt-16 bg-secondary-50 w-max p-2 ml-2 rounded max-w-[50%]'>
+    <div className='h-max'>
+    {/* <div className='mt-16 bg-secondary-50 w-max p-2 ml-2 rounded max-w-[50%]'>
       What you doing??
     </div>
     <div className='mt-5 bg-secondary-500 w-max p-2 ml-2 rounded max-w-[50%] absolute right-2'>
       kuch bhi nahii
+    </div> */}
+    <div className='flex flex-col h-screen'>
+    <div className='overflow-y-auto mt-16 flex flex-col flex-1'>
+        chat
     </div>
-    </>
+    <div className='overflow-y-auto flex flex-col flex-1'>
+        chat
+    </div>
+    </div>
+    </div>
   )
 }
 
