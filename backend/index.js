b7695c216e619f0a619d38b684d596cb9782fb83
AdityaBansal1402 <bansaladitya3009@gmail.com>
2024-06-04 17:05:45 +0530

    bruh

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
-const io = new Server({
+const express = require("express");
+const app=express();
+const http= require("http");
+const {Server}=require("socket.io");
+const cors=require("cors");
+
+app.use(cors());
+
+const server=http.createServer(app);
+
+const io = new Server(server, {
     cors: {
       origin: "http://localhost:3000"
     }
   });
+
   
-  io.listen(4000);
+  server.listen(4000,()=>{
+    console.log("server running at 4000 port");
+  })
